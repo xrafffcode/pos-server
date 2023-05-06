f84a7e367e55559b9a62b57b1a5c6ad78adbf30d
Muhamad Rafli <yusfiwawan2@gmail.com>
2023-05-06 14:13:33 +0700

    udpate

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ const port = 3000
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
+const cors = require('cors')
+app.use(cors())
+
+
 const db = require('./config/connection')
+
+const productRouter = require('./router/products');
+
+app.use('/products', productRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
